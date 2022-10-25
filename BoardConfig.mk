e28be1ce33f4f03fbb098db820c5f931ac109052
Eliminater74 <eliminater74@gmail.com>
2022-10-25 11:55:28 -0400

    BoardConfig: some TWRP Changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,20 @@ TARGET_BOARD_PLATFORM := tegra
 
 # TWRP Configuration
 
+# TWRP settings
+RECOVERY_VARIANT := twrp
+TW_THEME := landscape_hdpi
+TW_NO_SCREEN_TIMEOUT := true
+TW_INCLUDE_MTP := true
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/bq27742-0
+TW_INCLUDE_CRYPTO := true
+TARGET_RECOVERY_DEVICE_MODULES := fwtool
+TW_INCLUDE_NTFS_3G := true
+#TW_EXTRA_LANGUAGES := true
+
+
 # Recovery
 #TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -80,30 +94,20 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
-# Misc
-#TW_MTP_DEVICE := /dev/mtp_usb
-#TW_HAS_MTP := true
-#TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-
 # Debug
 TARGET_USES_LOGD := true
 # include Logcat daemon for help in debugging
 TWRP_INCLUDE_LOGCAT := true
 TWRP_EVENT_LOGGING := false
 
-# TWRP Theme BOARD_SUPER_PARTITION_GROUPS
-TW_THEME := landscape_hdpi
-# Old is gold. Put false to use the old one.
-#TWRP_NEW_THEME := true
-
 # Set the path to the sysfs entry which controls the brightness
-#TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_BRIGHTNESS_PATH := /sys/class/backlight/lpm102a188a-backlight/brightness
 
 # Max brightness to prevent display damage
-#TW_MAX_BRIGHTNESS := 255
+TW_MAX_BRIGHTNESS := 255
 
 # Default brightness for TWRP
-#TW_DEFAULT_BRIGHTNESS := 150
+TW_DEFAULT_BRIGHTNESS := 150
 
 # Disable/enable SELinux. Only suggested when you want to enable SELinux support
 #TWHAVE_SELINUX := true
@@ -117,19 +121,19 @@ TW_DEFAULT_LANGUAGE := en
 #BOARD_HAS_SDCARD_INTERNAL := true
 
 # TWRP specific build flags
-RECOVERY_VARIANT := twrp
+#RECOVERY_VARIANT := twrp
 #RECOVERY_SDCARD_ON_DATA := true
 #TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
 #TARGET_RECOVERY_PIXEL_FORMAT := RGBA_8888
 #TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 #TARGET_RECOVERY_FORCE_PIXEL_FORMAT := "RGB_565"
-BOARD_HAS_NO_SELECT_BUTTON := true
+#BOARD_HAS_NO_SELECT_BUTTON := true
 #BOARD_TOUCH_RECOVERY :=
-BOARD_RECOVERY_SWIPE := true
+#BOARD_RECOVERY_SWIPE := true
 #BOARD_SUPPRESS_SECURE_ERASE := true
 #TW_USE_NEW_MINADBD := true
-TW_USE_TOOLBOX := true
+#TW_USE_TOOLBOX := true
 
 #TW_INCLUDE_LOGICAL := product odm
 TW_EXTRA_LANGUAGES := false
@@ -142,12 +146,12 @@ TW_OEM_BUILD := true
 
 ### TW NO'S ###
 TW_NO_SCREEN_TIMEOUT := true
-TW_NO_HAPTICS := true
-TW_NO_LEGACY_PROPS := true
+#TW_NO_HAPTICS := true
+#TW_NO_LEGACY_PROPS := true
 # Disable the battery percentage for devices where it doesn't work properly
-TW_NO_BATT_PERCENT := true
+#TW_NO_BATT_PERCENT := true
 # Remove exFAT formatting binaries
-TW_NO_EXFAT := true
+#TW_NO_EXFAT := true
 
 # Decryption support for /data
 #TW_INCLUDE_CRYPTO := true
